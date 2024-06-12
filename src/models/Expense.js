381490dc488617240157e9c3f651163ce82f310a
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2024-06-12 16:43:14 +0530

    feat: created expense model schema

---
+++ src/models/Expense.js
@@ -0,0 +1,24 @@
+import mongoose, { Schema } from "mongoose";
+
+const ExpenseSchema = new Schema(
+  {
+    budgetId: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Budget",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    amount: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const ExpenseModel =
+  mongoose.models.Expense || mongoose.model("Expense", ExpenseSchema);
+
+export default ExpenseModel;
