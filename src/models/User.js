2d57782bf130bec380691accecc12be67746d8fb
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2024-06-13 10:48:44 +0530

    feat: created User model schema

---
+++ src/models/User.js
@@ -0,0 +1,25 @@
+import mongoose, { Schema } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+const UserModel = mongoose.models.User || mongoose.model("User", UserSchema);
+
+export default UserModel;
