7b94da602290a4499d294b4310d55812d59a5640
MohdFahad1 <98938137+MohdFahad1@users.noreply.github.com>
2024-06-13 12:26:08 +0530

    feat: Add endpoint for user registration

---
+++ src/app/api/sign-up/route.js
@@ -0,0 +1,69 @@
+import UserModel from "@/models/User";
+import { hashPassword } from "@/helpers/authHelper";
+import dbConnect from "@/lib/dbConnect";
+
+export async function POST(req) {
+  await dbConnect();
+
+  try {
+    const { username, email, password } = await req.json();
+
+    if (!username) {
+      return new Response(JSON.stringify({ error: "Username is required" }), {
+        status: 400,
+      });
+    }
+    if (!email) {
+      return new Response(JSON.stringify({ error: "Email is required" }), {
+        status: 400,
+      });
+    }
+    if (!password) {
+      return new Response(JSON.stringify({ error: "Password is required" }), {
+        status: 400,
+      });
+    }
+
+    const existingUser = await UserModel.findOne({ email });
+
+    if (existingUser) {
+      return new Response(
+        JSON.stringify({
+          success: false,
+          message: "Email already registered",
+        }),
+        { status: 400 }
+      );
+    }
+
+    const hashedPassword = await hashPassword(password);
+
+    const newUser = new UserModel({
+      username,
+      email,
+      password: hashedPassword,
+    });
+
+    await newUser.save();
+
+    return new Response(
+      JSON.stringify({
+        success: true,
+        message: "User registered successfully",
+        user: newUser,
+      }),
+      { status: 201 }
+    );
+  } catch (error) {
+    console.error("Error registering user: ", error);
+
+    return new Response(
+      JSON.stringify({
+        success: false,
+        message: "Something went wrong while registering user",
+        error: error.message,
+      }),
+      { status: 500 }
+    );
+  }
+}
